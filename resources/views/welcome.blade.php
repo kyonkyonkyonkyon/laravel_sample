8ddf7dc4dbab110a11d261e920492f50f9666680
kyonkyonkyonkyon <k.kusano.8.19@gmail.com>
2020-06-28 21:40:35 +0900

    add_practice

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: yellow;
+                background-color: blue;
                 color: #fff;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
