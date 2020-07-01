1ee0e173f999afd54455b3f22e58b5d91215ee57
kyonkyonkyonkyon <k.kusano.8.19@gmail.com>
2020-07-01 22:24:19 +0900

    add_practice

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel_practice
+                    practice
                 </div>
 
                 <div class="links">
